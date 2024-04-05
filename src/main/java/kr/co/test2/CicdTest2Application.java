7de47919c1ccf82f3f8d7c8dab5b80b56383907d
gahui446 <gahui446@gmail.com>
2024-04-05 09:44:00 +0900

    add

---
+++ src/main/java/kr/co/test2/CicdTest2Application.java
@@ -0,0 +1,13 @@
+package kr.co.test2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdTest2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdTest2Application.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/test2/CicdTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.test2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdTest2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
